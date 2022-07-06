d8c3fb49020816b2bc3ad91b327079f7588d0628
sh3ngsh3ng <yshenglim13@gmail.com>
2022-07-06 05:44:48 +0000

    added props to textareacomponent, state to textareacontainercomponent

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -5,7 +5,14 @@ export default function TextArea(props) {
     return (
         <React.Fragment>
             <div class="text-area">
-                <textarea readonly={props.read} class='text-box'>hello</textarea>
+                <textarea name={props.name} 
+                readonly={props.read} 
+                class='text-box' 
+                placeholder={props.placeholder}
+                onChange={props.updateState}
+                >
+                    {props.textDisplay}
+                </textarea>
             </div>
             
         </React.Fragment>
--- src/components/TextAreaContainer.js
+++ src/components/TextAreaContainer.js
@@ -3,17 +3,37 @@ import TextArea from './TextArea'
 import './TextAreaContainer.css'
 
 export class TextAreaContainer extends React.Component {
+
+    state = {
+        userInput: "",
+        userInputFormatted: ""
+    }
+
+    updateState = (evt) => {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+
     render() {
         return (
             <React.Fragment>
                 <div id="main-container">
-                    <TextArea />
+                    <TextArea 
+                    name="userInput" 
+                    textDisplay={this.state.userInput} 
+                    updateState={this.updateState} 
+                    placeholder="Paste your JSON over here!"/>
                     <div>
-                        <button>1</button>
+                        <button id="btn-1">1</button>
                         <p></p>
                         <button>2</button>
                     </div>
-                    <TextArea />    
+                    <TextArea 
+                    name="userOutput" 
+                    textDisplay={this.state.userInputFormatted} 
+                    placeholder="Formatted JSON will be displayed here!"
+                    read='true' />    
                 </div>
             </React.Fragment>
 
